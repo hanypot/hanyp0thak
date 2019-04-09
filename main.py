0b9dcd1d5b8b6bb3d9d72b57e1affbc7e8616a55
hanypot <ivan.hrabcak@gmail.com>
2019-04-09 19:48:34 +0200

    Comments, reorder
    xd

--- main.py
+++ main.py
@@ -4,28 +4,30 @@ import os # magic
 import sys # magic
 import socket # magic
 
-window = Tk()
-window.title("HanyPot xd")
-window.geometry('250x100')
-
-
-
-lbl = Label(window, text = "u gay m8")
-lbl.grid(row = 0, column = 0)
-
-
-
-text = StringVar()
-cmd_let = Entry(window, textvariable = text)
-cmd_let.grid(row=3, column=5)
-
+window = Tk() # vytváram okno
+window.title("HanyPot xd") # nadpis na okne
+window.geometry('250x100') # výška a šírka okna
 
 def clicked():
-	klik = Label(window, text = "klik m8")
+	klik = Label(window, text = "klik m8") # xd
 	klik.grid(row = 2, column = 0)
+	print("bruh wtf...")
 	os.system(text.get())
+
 
 btn = Button(window, text = "Init", command = clicked)
 btn.grid(row = 5, column = 0)
+
+lbl = Label(window, text = "u gay m8") # nápis U GAY M8
+lbl.grid(row = 0, column = 0)
+
+print("bruh wtf...")
+
+text = StringVar()
+cmd_let = Entry(window, textvariable = text) # text field
+cmd_let.grid(row=3, column=5)
+
+
+
 
 window.mainloop()
