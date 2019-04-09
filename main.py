c529fb1c41759715383c806ab929bbba85ba134f
hanypot <ivan.hrabcak@gmail.com>
2019-04-09 19:54:05 +0200

    Update main.py

--- main.py
+++ main.py
@@ -3,7 +3,14 @@ from tkinter import * # ui
 import os # magic
 import sys # magic
 import socket # magic
+"""
+TODO
+-zisti ake slabiny ma vargov hanypot
+-urob z tohoto este vacsi meme jak to je            -half-DONE
+- xd                                                -DONE
 
+
+"""
 window = Tk() # vytváram okno
 window.title("HanyPot xd") # nadpis na okne
 window.geometry('250x100') # výška a šírka okna
@@ -11,6 +18,8 @@ window.geometry('250x100') # výška a šírka okna
 def clicked():
 	klik = Label(window, text = "klik m8") # xd
 	klik.grid(row = 2, column = 0)
+	bruh1 = Label(window, text = "bruh").grid(row = 6, column = 1)
+	bruh2 = Label(window, )
 	print("bruh wtf...")
 	os.system(text.get())
 
@@ -21,7 +30,6 @@ btn.grid(row = 5, column = 0)
 lbl = Label(window, text = "u gay m8") # nápis U GAY M8
 lbl.grid(row = 0, column = 0)
 
-print("bruh wtf...")
 
 text = StringVar()
 cmd_let = Entry(window, textvariable = text) # text field
