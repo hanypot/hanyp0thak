1d2734a615a15635cb97e65f9d82500e3c3f6f36
hanypot <ivan.hrabcak@gmail.com>
2019-04-09 20:26:01 +0200

    Actually fix it

--- main.py
+++ main.py
@@ -3,6 +3,7 @@ from tkinter import * # ui
 import os # magic
 import sys # magic
 import socket # magic
+import time
 """
 TODO
 -zisti ake slabiny ma vargov hanypot
@@ -10,23 +11,16 @@ TODO
 """
 window = Tk() # vytváram okno
 window.title("xd") # nadpis na okne
-window.geometry('200x200') # výška a šírka okna
+window.geometry('400x100') # výška a šírka okna
 
 def clicked():
-	clickedtxt = Label(window, text = "done") # xd
-	clickedtxt.grid(row = 7, column = 6)
 	os.system(text.get())
 
-
 btn = Button(window, text = "Init", command = clicked)
-btn.grid(row = 0, column = 10)
-
+btn.grid(row = 20, column = 0)
 
 text = StringVar()
 cmd_let = Entry(window, textvariable = text) # text field
 cmd_let.grid(row=10, column=0)
-
-
-
 
 window.mainloop()
