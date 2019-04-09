888789b2819c637e128d9440f38a1f64e8d72d3d
hanypot <ivan.hrabcak@gmail.com>
2019-04-09 20:18:45 +0200

    Actually fix them

--- main.py
+++ main.py
@@ -6,31 +6,25 @@ import socket # magic
 """
 TODO
 -zisti ake slabiny ma vargov hanypot
--urob z tohoto este vacsi meme jak to je            -half-DONE
 - xd                                                -DONE
-
-
 """
 window = Tk() # vytváram okno
-window.title("HanyPot xd") # nadpis na okne
-window.geometry('250x100') # výška a šírka okna
+window.title("xd") # nadpis na okne
+window.geometry('200x200') # výška a šírka okna
 
 def clicked():
 	clickedtxt = Label(window, text = "done") # xd
-	clickedtxt.grid(row = 6, column = 6)
+	clickedtxt.grid(row = 7, column = 6)
 	os.system(text.get())
 
 
 btn = Button(window, text = "Init", command = clicked)
-btn.grid(row = 5, column = 0)
-
-lbl = Label(window, text = "hanyP0t") # nápis
-lbl.grid(row = 0, column = 0)
+btn.grid(row = 0, column = 10)
 
 
 text = StringVar()
 cmd_let = Entry(window, textvariable = text) # text field
-cmd_let.grid(row=3, column=5)
+cmd_let.grid(row=10, column=0)
 
 
 
