9dc915abd570a8a4e265a684c19e19e16686cf70
hanypot <ivan.hrabcak@gmail.com>
2019-04-10 20:02:37 +0200

    Made the whole portscanner to a function

--- portscanner.py
+++ portscanner.py
@@ -1,43 +1,16 @@
 import socket
-import sys
-from datetime import datetime
 
-
-host = input("[*] Enter a remote host to scan: ")
-hostIP  = socket.gethostbyname(host)
-
-print("-" * 60)
-print("[*] Please wait, scanning remote host", hostIP)
-print("-" * 60)
-
-
-t1 = datetime.now()
-
-try:
-    for port in range(1,1025):  
-        sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-        result = sock.connect_ex((hostIP, port))
-        if result == 0:
-            print("Port {}: 	 Open".format(port))
-        sock.close()
-
-except KeyboardInterrupt:
-    print("\n[-] KeyboardInterrupt")
-    sys.exit()
-
-except socket.gaierror:
-    print('[-] Hostname could not be resolved. Exiting...')
-    sys.exit()
-
-except socket.error:
-    print("[-] Couldn't connect to server")
-    sys.exit()
-
-# Checking the time again
-t2 = datetime.now()
-
-# Calculates the difference of time, to see how long it took to run the script
-total =  t2 - t1
-
-# Printing the information to screen
-print('[*] Scanning Completed in: ', total)
+def scan(host, port):
+	sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+	try:
+		result = sock.connect_ex((host, port))
+		if result == 0:
+			return(True)
+			sock.close()
+		else:
+			return(False)
+			sock.close()
+	except socket.error:
+		pass
+	except socket.gaierror:
+		pass
